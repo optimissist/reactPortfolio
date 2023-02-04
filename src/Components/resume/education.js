99953b6c85ef8643f82094897e02f8a76c52095b
Marla <marla.schevker@gmail.com>
2023-02-04 13:18:06 -0800

    updated experience files

--- src/Components/resume/education.js
+++ src/Components/resume/education.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 export default function Education() {
     return (
-    <div className="certs">
-        
-        <ul>
-            <li><a href="./images/SchevkerResume.pdf" target="_blank"> My Resume </a></li>
+    <div className="education">
+
+        <ul className="educationList">
             <li> Front-end Engineer Path Certification - Codecademy</li>
-            <li><a href="./images/JavaScript.pdf" target="_blank">JavaScript Certification</a> - OneMonth</li>
-            <li><a href="./images/jQuery.pdf" target="_blank">jQuery Certification</a> - OneMonth</li>
-            <li><a href="./images/ResponsiveDesign.pdf" target="_blank">Responsive Resign Certification</a> - OneMonth</li>
-            <li><a href="./images/Wordpress.pdf" target="_blank">Wordpress Certification</a> - OneMonth</li>
-            <li> BA - 2010 - California State University Northridge</li>
             <li> MA - 2014 - University of Southern California </li>
+            <li> BA - 2010 - California State University Northridge</li>
         </ul>
     </div>
     )
--- src/Components/resume/resume.js
+++ src/Components/resume/resume.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './resume.css';
 import Skills from './skills.js'
 import Experience from './experience/experience.js'
 import Education from './education.js'
 
 export default function Resume() {
+    const [experience, setExperience] = useState(false);
+    const [skills, setSkills] = useState(false);
+    const [eduation, setEducation] = useState(false);
+
     return (
         <div className="resume">
-            <Experience />
-            <Skills />
-            <Education />
+            <p className="experienceTitle" onClick={() => setExperience(!experience)}>Experience</p>
+                {experience ?  <Experience /> : ""}
+            <p className="skillsTitle" onClick={() => setSkills(!skills)} >Skills</p>
+                {skills ? <Skills /> : ""}
+            <p className="educationTitle" onClick={() => setEducation(!eduation)}>Education</p>
+            {eduation ? <Education /> : ""}
         </div>
     )
     }
